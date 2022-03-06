2607618a52277e168ad3113d113878d5f72345a8
nl10001 <98903190+nl10001@users.noreply.github.com>
2022-03-06 14:55:32 +0000

    fixing task 4/5, adding tasks 7 and 8

--- Assignment2/Assignment2.ino
+++ Assignment2/Assignment2.ino
@@ -15,17 +15,27 @@
 #define t_signal 1
 #define NOS_TASKS 4
 #define MAX_VOLTAGE 4095
+#define POW(i) pow(10,i)
 
 // initialising the button state variables to low (default)
 int button1State = 0;
 int button2State = 0;
 
 //int ticks = 0;
-int counter = 0;
+int counter_main = 0;
+int counter_task4_5 = 0;
 int analog_in = 0;
+int average_an;
 int wave_freq = 0;
 int pinData = 0;
 int i = 0;
+int data4 = 0;
+int sum = 0;
+int task7_data = 0;
+int task8_data = 0;
+int error_code = 0;
+
+int data_array[] = {0,0,0,0};
 
 Ticker Cycle;
 
@@ -51,35 +61,55 @@ void loop() {}
 
 //cyclic executive function
 void cycleF() {
-  counter++;
+  counter_main++;
   //digitalWrite(ledPin2, HIGH);
   task1(); // every cycle
   //digitalWrite(ledPin2, LOW);
   //Serial.println("1");
   
-  if ((counter % 200) == 0){
+  if ((counter_main % 200) == 0){
     //digitalWrite(ledPin2, HIGH);
     task2(); // every 200ms (5Hz)
     //digitalWrite(ledPin2, LOW);
     //Serial.println("2");
   }
   
-  if ((counter % 1000) == 0) {
+  if ((counter_main % 1000) == 0) {
     task3(); // every 1000ms (1Hz)
   }
   
-  if ((counter % 42) == 0) {
+  if ((counter_main % 42) == 0) {
     //digitalWrite(ledPin2, HIGH);
-    task4();
+    data4 = task4();
     //digitalWrite(ledPin2, LOW);
-    
   }
-  digitalWrite(ledPin2, HIGH);
+  
+  if ((counter_main % 42) == 21) {
+    //digitalWrite(ledPin2, HIGH);
+    task7_data = task5(data4);
+    //digitalWrite(ledPin2, LOW);
+  }
+/*
+  if ((counter_main % 21) == 0) {
+    counter_task5_6++;
+    if ((counter_task % 2) == 0) {
+      
+    }
+  }
+ */ 
   for(i = 0; i < 10; i++) {
     task6();
   }
-  digitalWrite(ledPin2, LOW);
-  if((counter % 5000) == 0) {
+
+  if((counter_main % 333) == 0) {
+    task8_data = task7(task7_data);  
+  }
+
+  if((counter_main % 333) == 167) {
+    task8(task8_data);
+  }
+
+  if((counter_main % 5000) == 0) {
     task9();
   }
   
@@ -112,23 +142,52 @@ void task2() {
 //Measure frequency of 3.3V square wave signal
 void task3() {
   pinData = pulseIn(PIN_4, LOW);
-  wave_freq = 1/(2*pinData*pow(10,-6));
+  wave_freq = 1/(2*pinData*POW(-6));
   //Serial.println(wave_freq);
 }
 
-void task4() {
+int task4() {
   analog_in = analogRead(AN_PIN_0);
   //Serial.println(analog_in);
-  /*
-  Serial.println("test");
-  digitalWrite(ledPin2, HIGH);
-  delayMicroseconds(100);
-  digitalWrite(ledPin2, LOW);
-  */
+  return analog_in;
 }
 
-void task5() {
+int task5(int data4) {
   
+  //Serial.println(counter_task4_5);
+  switch(counter_task4_5) {
+    case 0:
+      data_array[counter_task4_5] = data4;
+      break;
+    case 1:
+      data_array[counter_task4_5] = data4;
+      break;
+    case 2:
+      data_array[counter_task4_5] = data4;
+      break;
+    case 3:
+      data_array[counter_task4_5] = data4;
+      break;
+  }
+  counter_task4_5++;
+  /*
+  for(i = 0; i < 4; i++) {
+    Serial.print(data_array[i]);
+    Serial.print("\t");
+    
+  }
+  Serial.println();
+  */
+  if(counter_task4_5 == 4) {
+    counter_task4_5 = 0;
+  }
+  sum = 0;
+  for(i = 0; i < 4; i++) {
+    sum += data_array[i];
+  }
+  average_an = sum/4;
+  Serial.println(average_an);
+  return average_an;
 }
 
 //do nothing tasks set to run 10 times per ms rather than 1000 times every 100 ms
@@ -141,11 +200,23 @@ void task6() {
   
 }
 
-void task7() {
-  //if (average_analogue_in > (MAX_VOLTAGE/2))
+int task7(int data) {
+  if (data > (MAX_VOLTAGE/2)) {
+    error_code = 1;
+  }
+  else {
+    error_code = 0;
+  }
+  return error_code;
 }
 
-void task8() {
+void task8(int data) {
+  if(data == 1) {
+    digitalWrite(ledPin2, HIGH);
+  }
+  else {
+    digitalWrite(ledPin2, LOW);
+  }
   
 }
 
